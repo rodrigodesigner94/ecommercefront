096e24619925a8a21c6d5633f6e485974f5f65ae
Rodrigo Borges <digodesign94@gmail.com>
2022-05-03 19:44:31 -0300

    global style

---
+++ src/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+margin: 0;
+padding: 0;
+
+height: 100vh;
+font-family: sans-serif;
+background-image: linear-gradient(to top, #fff, #5ddcf9ff, #013dd5ff);
+
+`
